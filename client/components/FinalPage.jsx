9a41b792cceea78d0eae28f832da1b646b91dc7b
max-tweddell <max-tweddell>
2017-07-28 11:24:32 +1200

    formmatting

--- client/components/FinalPage.jsx
+++ client/components/FinalPage.jsx
@@ -4,8 +4,8 @@ import Score from './Score'
 
 function finalPage (props) {
   render(
-    <div>
-    <h1> Finnishd </h1>
+    <div className="finalPage">
+      <h1> Finnishd </h1>
     <Score {props}/>
     </div>
   )
