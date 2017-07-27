136b9852a6d60ab6fbbcec830a286bc81d6a7b77
max-tweddell <max-tweddell>
2017-07-28 10:07:57 +1200

    added basic score function

---
+++ client/components/Score.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/*
+this needs to be passed down props which contain the Score
+*/
+function Score (props) {
+  score = props.score
+  return (
+    <div><h3> Your score is ${score}/10`</h3></div>
+  )
+
+}
+
+export defaults score
