32dbb73dd407e95991a239beded06305771e2a7e
max-tweddell <max-tweddell>
2017-07-28 11:02:48 +1200

    added error log)

--- client/components/Score.jsx
+++ client/components/Score.jsx
@@ -9,6 +9,7 @@ function Score (props) {
   else if (score < 6) {text = textArray[1]}
   else if (score < 8) {text = textArray[2]}
   else if (score < 10) {text = textArray[3]}
+  else {console.log("Error in the score component!")}
   const score = props.score
   return (
     <div className="scoreBox">
