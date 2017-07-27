3560f5c9be5dd3494298c914e3d8f5209a250fbb
joshua-fogg <joshuafoggnz@gmail.com>
2017-07-28 10:36:51 +1200

    add mock questions to state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,7 +13,23 @@ class App extends React.Component {
     super(props)
     this.state = {
       score: 0, //pass to score
-      
+      questions: [
+        { id: 1, question: 'stuff', person: 'josh', pic1: '#', pic2: '#', pic3: '#', pic4: '#' },
+        { id: 2, question: 'stuff', person: 'josh' },
+        { id: 3, question: 'stuff', person: 'james' },
+        { id: 4, question: 'stuff', person: 'james' },
+        { id: 5, question: 'stuff', person: 'seth' },
+        { id: 6, question: 'stuff', person: 'seth' },
+        { id: 7, question: 'stuff', person: 'emma' },
+        { id: 8, question: 'stuff', person: 'emma' },
+        { id: 9, question: 'stuff', person: 'max' },
+        { id: 10, question: 'stuff', person: 'max' },
+        { id: 11, question: 'stuff', person: 'ali' },
+        { id: 12, question: 'stuff', person: 'ali' },
+        { id: 13, question: 'stuff', person: 'nathan' },
+        { id: 14, question: 'stuff', person: 'nathan' }
+    ],
+      persons: [],
     }
   }
 
