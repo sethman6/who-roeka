026c4bf0dfaf92f0577b6786d8219c84a81c3a79
Nathan Lamb <nathanlamb022@gmail.com>
2017-07-28 10:36:17 +1200

    Add questions

--- public/data/questions.js
+++ public/data/questions.js
@@ -1,16 +1,12 @@
-export const questions [
-	{ id: 1, question: 'stuff', person: 'josh' },
-	{ id: 2, question: 'stuff', person: 'josh' },
-	{ id: 3, question: 'stuff', person: 'james' },
-	{ id: 4, question: 'stuff', person: 'james' },
-	{ id: 5, question: 'stuff', person: 'seth' },
-	{ id: 6, question: 'stuff', person: 'seth' },
-	{ id: 7, question: 'stuff', person: 'emma' },
-	{ id: 8, question: 'stuff', person: 'emma' },
-	{ id: 9, question: 'stuff', person: 'max' },
-	{ id: 10, question: 'stuff', person: 'max' },
-	{ id: 11, question: 'stuff', person: 'ali' },
-	{ id: 12, question: 'stuff', person: 'ali' },
-	{ id: 13, question: 'stuff', person: 'nathan' },
-	{ id: 14, question: 'stuff', person: 'nathan' }
+export const questions = [
+	{ id: 1, question: 'Who does Duke the elderly cat belong to? ', person: 'james' },
+	{ id: 2, question: 'This person spent a night locked in bar', person: 'seth' },
+	{ id: 3, question: 'The father of who was awarded an Emmy?', person: 'emma' },
+	{ id: 4, question: 'Who owns nearly 2 dozen critters of the Superclass Osteichthyes?', person: 'max' },
+	{ id: 5, question: 'Who has been close enough to Chris Hemsworth to touch him?', person: 'josh' },
+	{ id: 6, question: 'This person has eaten various insects', person: 'seth' },
+	{ id: 7, question: 'Who was born on an island in Asia?', person: 'james' },
+	{ id: 8, question: 'This person hates shellfish especially icky oysters', person: 'emma' },
+	{ id: 9, question: 'Who is licensed to zoom zoom on a motorcycle?', person: 'max' },
+	{ id: 10, question: 'Who comes from the place where the neolithic revolution ocurred 10 000 years ago?', person: 'ali' }
 ]
