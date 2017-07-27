9857bfc71188a126439f71512f05873955d87c45
joshua-fogg <joshuafoggnz@gmail.com>
2017-07-28 09:59:08 +1200

    correct question.js

--- public/data/questions.js
+++ public/data/questions.js
@@ -1,16 +1,16 @@
-export const questions [
-	{ id: 1, question: 'stuff', person: 'josh' },
-	{ id: 2, question: 'stuff', person: 'josh' },
-	{ id: 3, question: 'stuff', person: 'james' },
-	{ id: 4, question: 'stuff', person: 'james' },
-	{ id: 5, question: 'stuff', person: 'seth' },
-	{ id: 6, question: 'stuff', person: 'seth' },
-	{ id: 7, question: 'stuff', person: 'emma' },
-	{ id: 8, question: 'stuff', person: 'emma' },
-	{ id: 9, question: 'stuff', person: 'max' },
-	{ id: 10, question: 'stuff', person: 'max' },
-	{ id: 11, question: 'stuff', person: 'ali' },
-	{ id: 12, question: 'stuff', person: 'ali' },
-	{ id: 13, question: 'stuff', person: 'nathan' },
-	{ id: 14, question: 'stuff', person: 'nathan' }
+export const questions = [
+    { id: 1, question: 'stuff', person: 'josh' },
+    { id: 2, question: 'stuff', person: 'josh' },
+    { id: 3, question: 'stuff', person: 'james' },
+    { id: 4, question: 'stuff', person: 'james' },
+    { id: 5, question: 'stuff', person: 'seth' },
+    { id: 6, question: 'stuff', person: 'seth' },
+    { id: 7, question: 'stuff', person: 'emma' },
+    { id: 8, question: 'stuff', person: 'emma' },
+    { id: 9, question: 'stuff', person: 'max' },
+    { id: 10, question: 'stuff', person: 'max' },
+    { id: 11, question: 'stuff', person: 'ali' },
+    { id: 12, question: 'stuff', person: 'ali' },
+    { id: 13, question: 'stuff', person: 'nathan' },
+    { id: 14, question: 'stuff', person: 'nathan' }
 ]
