9ce1d6d14cc89d4175c15b0a56af92477bf62cba
max-tweddell <max-tweddell>
2017-07-28 11:17:24 +1200

    refactoring

--- client/components/Score.jsx
+++ client/components/Score.jsx
@@ -10,7 +10,7 @@ function Score (props) {
   else if (score < 8) {text = textArray[2]}
   else if (score < 10) {text = textArray[3]}
   else {console.log("Error in the score component!")}
-  const score = props.score
+  let score = props.score
   return (
     <div className="scoreBox">
       <h3> `Your score is ${score}/10`</h3>
