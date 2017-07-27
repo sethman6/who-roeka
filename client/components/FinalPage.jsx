3bea660425c4c90951c2cba2795c285599bf4a16
max-tweddell <max-tweddell>
2017-07-28 11:20:50 +1200

    added final page

---
+++ client/components/FinalPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Score from './Score'
+
+
+function finalPage (props) {
+  render(
+    <div>
+    <h1> Finnishd </h1>
+    <Score {props}/>
+    </div>
+  )
+}
+
+export defaults finalPage
