ded875e6667a978adccfce0265d7a8d6d2a0201a
max-tweddell <max-tweddell>
2017-07-28 12:12:05 +1200

    formatting

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Route, Switch, HashRouter as Router } from 'react-router-dom'
-import { render } from 'react-dom'
+import {Route, Switch, HashRouter as Router} from 'react-router-dom'
+import {render} from 'react-dom'
 import questions from '../../public/data/questions'
 import people from '../../public/data/people'
 
@@ -16,29 +16,34 @@ class App extends React.Component {
     this.state = {
       score: 0, //pass to score
       questions: questions,
-      people: people,
+      people: people
+    }
+    this.incrementScore = this.incrementScore.bind(this)
   }
-  this.incrementScore=this.incrementScore.bind(this)
-  }
-  incrementScore(){
-   this.setState({score: this.state.score+1})
+  incrementScore() {
+    this.setState({
+      score: this.state.score + 1
+    })
   }
 
   render() {
     return (
       <Router>
-        <div className='content'>
-          <h1>Who-roeka Quiz 2017 Edition</h1>
-          <Route exact path='/' component={MainPage} />
-          <Route path='/quiz/:qid' render={(routerProps) => {
-            console.log(routerProps)
-            const qid = Number(routerProps.match.params.qid)
-            const question = this.state.questions.find((question)=> question.id===qid) 
-            return <Question {...routerProps} people={this.state.people} question={question} incrementScore={this.incrementScore} numQuestions={this.state.questions.length}/>
-          }
-          } />
-        </div>
-     
+      <div className='content'>
+      <h1>Who-roeka Quiz 2017 Edition</h1>
+      <Route exact path='/' component={MainPage}/>
+      <Route path='/quiz/:qid' render={(routerProps) => {
+        console.log(routerProps)
+        const qid = Number(routerProps.match.params.qid)
+        const question = this.state.questions.find((question) => question.id === qid)
+        return <Question {...routerProps}
+          people={this.state.people}
+          question={question}
+          incrementScore={this.incrementScore}
+          numQuestions={this.state.questions.length}/>
+      }}/>
+      </div>
+
       </Router>
     )
   }
